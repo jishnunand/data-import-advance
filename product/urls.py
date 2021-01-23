a57b65587eb43b8e6621b64a7639163f9e3da278
Jishnunand <jishnunand@gmail.com>
2021-01-23 16:14:40 +0530

    added product list pages and data listing API with datatable too

--- product/urls.py
+++ product/urls.py
@@ -3,4 +3,5 @@ from product import views
 
 urlpatterns = [
     path('', views.product_list_view, name='product-list'),
+    path('data/list/', views.ProductListJson.as_view(), name='product-list-data-json'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django_datatables_view.base_datatable_view import BaseDatatableView
+
+from product.filters import ProductFilter
+from product.models import Product
 
 
 def product_list_view(request):
@@ -9,3 +13,25 @@ def product_list_view(request):
     """
     context = dict()
     return render(request, 'product/index.html', context=context)
+
+
+class ProductListJson(BaseDatatableView):
+    """
+
+    """
+    # The model we're going to show
+    model = Product
+
+    # define the columns that will be returned
+    columns = ['name', 'sku', 'id']
+
+    # define column names that will be used in sorting
+    # order is important and should be same as order of columns
+    # displayed by datatables. For non sortable columns use empty
+    # value like ''
+    order_columns = ['name', 'sku', 'id']
+
+    # set max limit of records returned, this is used to protect our site if someone tries to attack our site
+    # and make it return huge amount of data
+    max_display_length = 500
+
