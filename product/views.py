dad7f4ac00a064d0ecd9cad673e01ec99d05237d
Jishnunand <jishnunand@gmail.com>
2021-01-27 10:10:16 +0530

    fixed table search

--- product/views.py
+++ product/views.py
@@ -1,4 +1,6 @@
 import base64
+import codecs
+import csv
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
@@ -10,6 +12,7 @@ from product.filters import ProductFilter
 from product.models import Product
 from product.forms import ProductForm, ProductBulkImportForm
 from django.urls import reverse
+from django.db.models import Q
 
 
 def product_list_view(request):
@@ -20,6 +23,15 @@ def product_list_view(request):
     """
     context = dict()
     return render(request, 'product/index.html', context=context)
+
+
+def clean_filter_dict(filter_set):
+   """
+   Clean the dictionary for before sending to queryset filter
+   """
+   return {k: v for k, v in filter_set.items() if not 'columns' in k
+           and not 'order[' in k and not 'draw' in k and not
+           'search[' in k and not 'length' in k}
 
 
 class ProductListJson(BaseDatatableView):
@@ -36,11 +48,50 @@ class ProductListJson(BaseDatatableView):
     # order is important and should be same as order of columns
     # displayed by datatables. For non sortable columns use empty
     # value like ''
-    order_columns = ['edit', 'delete', 'name', 'sku', 'id']
+    order_columns = ['name', 'sku', 'id']
 
     # set max limit of records returned, this is used to protect our site if someone tries to attack our site
     # and make it return huge amount of data
     max_display_length = 500
+
+    # def ordering(self, qs):
+    #     """ Get parameters from the request and prepare order by clause
+    #         """
+    #
+    #     # Number of columns that are used in sorting
+    #     sorting_cols = 0
+    #     sort_key = 'order[{0}][column]'.format(sorting_cols)
+    #     while sort_key in self._querydict:
+    #         sorting_cols += 1
+    #         sort_key = 'order[{0}][column]'.format(sorting_cols)
+    #
+    #     order = []
+    #
+    #     order_columns = self.columns
+    #
+    #     for i in range(sorting_cols):
+    #         # sorting column
+    #         sort_dir = 'asc'
+    #         try:
+    #             sort_col = int(self._querydict.get(
+    #                 'order[{0}][column]'.format(i)))
+    #             # sorting order
+    #             sort_dir = self._querydict.get('order[{0}][dir]'.format(i))
+    #         except ValueError:
+    #             sort_col = 0
+    #
+    #         sdir = '-' if sort_dir == 'desc' else ''
+    #         sortcol = order_columns[sort_col]
+    #
+    #         if isinstance(sortcol, list):
+    #             for sc in sortcol:
+    #                 order.append('{0}{1}'.format(sdir, sc.replace('.', '__')))
+    #         else:
+    #             order.append('{0}{1}'.format(sdir, sortcol.replace('.', '__')))
+    #     print(order)
+    #     if order:
+    #         return qs.order_by(*order)
+    #     return qs
 
     def render_column(self, row, column):
         if column == "edit":
@@ -63,6 +114,24 @@ class ProductListJson(BaseDatatableView):
                 dictionary[column] = self.render_column(item, column)
             data.append(dictionary)
         return data
+
+    def filter_queryset(self, qs):
+        # use parameters passed in GET request to filter queryset
+        q = None
+        or_query = None
+        search = self.request.GET.get(u'search[value]', None)
+
+        if search:
+            model_field = ['name', 'sku', 'id']
+            for each_field in model_field:
+                q = Q(**{"%s__icontains" % each_field: search})
+                if or_query is None:
+                    or_query = q
+                else:
+                    or_query = or_query | q
+            qs = qs.filter(or_query)
+
+        return qs
 
 
 @login_required
@@ -113,7 +182,7 @@ def product_data_delete(request, product_id):
     context = {"message": "Requested data removed successfully"}
     return JsonResponse(context)
 
-import binascii
+
 def upload_file(request):
     """
 
@@ -124,8 +193,12 @@ def upload_file(request):
     form = ProductBulkImportForm(request.POST or None, request.FILES or None)
     if request.method == 'POST' and form.is_valid():
 
-        # print(request.FILES['file_upload'].file.name)
-        handle_uploaded_file.delay(request.FILES['file_upload'].file.name)
+        print(request.FILES['file_upload'].name)
+        file_name = "/tmp/{0}".format(request.FILES['file_upload'].name)
+        with open(file_name, 'wb+') as destination:
+            for chunk in request.FILES['file_upload'].chunks():
+                destination.write(chunk)
+        handle_uploaded_file.delay(file_name)
         return redirect('product-list')
     context['form'] = form
     return render(request, 'product/bulk-import.html', context=context)
@@ -133,8 +206,15 @@ def upload_file(request):
 
 @task(name="handle_uploaded_file")
 def handle_uploaded_file(encoded_file):
-    print("hello")
-    print(type(encoded_file))
-    with open(encoded_file, "r") as f:
-        print(f)
-
+    with codecs.open(encoded_file, mode='r', encoding='utf8') as csvfile:
+        data = csv.DictReader(csvfile, delimiter=",")
+        upload_data = list()
+        for row in data:
+            if len(upload_data) == 1000:
+                Product.objects.bulk_create(upload_data, ignore_conflicts=True)
+                upload_data = list()
+                upload_data.append(Product(**row))
+            else:
+                upload_data.append(Product(**row))
+        if upload_data:
+            Product.objects.bulk_create(upload_data, ignore_conflicts=True)
